e9cb4450c23c9c069f2be8de43737f057eb351f9
Justin Deloach <jadeloach01@gmail.com>
2019-08-27 00:27:32 -0400

    added whatFor method

--- Acme Systems/src/ver1/HellowWorld.java
+++ Acme Systems/src/ver1/HellowWorld.java
@@ -12,6 +12,10 @@ public class HellowWorld {
 		String Msg = "Hey Now!";
 		System.out.println(Msg);
 	}
+	public static void whatFor() {
+		String Msg = "What For?!";
+		System.out.println(Msg);
+	}
 
 
 }
